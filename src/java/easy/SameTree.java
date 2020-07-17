aab073b3d0b787c66304040d1c66a64235b0b193
rosaunde <rosaunde@tcd.ie>
2020-07-17 17:42:19 +0100

    couple more

---
+++ src/java/easy/SameTree.java
@@ -0,0 +1,19 @@
+package easy;
+
+
+import easy.util.TreeNode;
+
+public class SameTree {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        if(p.val != q.val){
+            return false;
+        }
+        return isSameTree(p.right,q.right) && isSameTree(p.left,q.left);
+    }
+}
