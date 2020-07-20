23c209837ab454890419b40a8f8f11008bfc7af1
rosaunde <rosaunde@tcd.ie>
2020-07-20 11:22:52 +0100

    Monday problem

---
+++ src/java/easy/SymmetricTree.java
@@ -0,0 +1,24 @@
+package easy;
+
+import easy.util.TreeNode;
+
+public class SymmetricTree {
+    public static boolean isSymmetric(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+    public static boolean isSymmetric(TreeNode left, TreeNode right) {
+        if(left == null && right == null){
+            return true;
+        }
+        if(left == null || right == null){
+            return false;
+        }
+        if(right.val != left.val){
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
