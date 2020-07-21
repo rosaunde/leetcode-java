b4945afc889f0529b3ec1c2da1124d35f80cd98a
rosaunde <rosaunde@tcd.ie>
2020-07-21 09:38:17 +0100

    Bluesday

---
+++ src/java/easy/BinaryTreeDepth.java
@@ -0,0 +1,13 @@
+package easy;
+
+import easy.util.TreeNode;
+
+public class BinaryTreeDepth {
+    public static int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        return Math.max(1+ maxDepth(root.left), 1 + maxDepth(root.right));
+    }
+}
---
+++ src/java/easy/LevelOrderBottom.java
@@ -0,0 +1,36 @@
+package easy;
+
+import easy.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LevelOrderBottom {
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<>();
+        return levelOrderBottom(Collections.singletonList(root), levels);
+    }
+
+    public static List<List<Integer>> levelOrderBottom(List<TreeNode> nodes, List<List<Integer>> levels) {
+        List<Integer> level = new ArrayList();
+        List<TreeNode> nextNodes = new ArrayList();
+        boolean hit = false;
+        for(int i = 0; i < nodes.size(); i++){
+            TreeNode node = nodes.get(i);
+            if(node != null){
+                level.add(node.val);
+                nextNodes.add(node.left);
+                nextNodes.add(node.right);
+                hit = true;
+            }
+        }
+        if(hit == false){
+            return levels;
+        }
+        else{
+            levels.add(0, level);
+            return levelOrderBottom(nextNodes, levels);
+        }
+    }
+}
