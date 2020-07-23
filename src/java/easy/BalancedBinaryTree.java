0bf5b4491223aadf84b70b26e0bd9b617993bb54
rosaunde <rosaunde@tcd.ie>
2020-07-23 11:31:48 +0100

    Thor's Day

---
+++ src/java/easy/BalancedBinaryTree.java
@@ -0,0 +1,35 @@
+package easy;
+
+import easy.util.TreeNode;
+
+public class BalancedBinaryTree {
+    public static boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        if(root.left == null && root.right == null){
+            return true;
+        }
+
+        int leftHeight = getHeight(root.left);
+        System.out.println(leftHeight);
+        int rightHeight = getHeight(root.right);
+        System.out.println(rightHeight);
+
+        if(leftHeight == rightHeight || leftHeight - rightHeight == 1 || leftHeight - rightHeight == -1){
+            return isBalanced(root.left) && isBalanced(root.right);
+        }
+
+        return false;
+    }
+
+    public static int getHeight(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null && root.right == null){
+            return 1;
+        }
+        return Math.max(1+getHeight(root.left), 1+getHeight(root.right));
+    }
+}
