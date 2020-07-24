71bbfc8bdbb5b3cee278139b42d7cf50d2a3166e
rosaunde <rosaunde@tcd.ie>
2020-07-24 20:34:52 +0100

    cheeky extra one, emma wasn't happy

---
+++ src/java/easy/SumRootToLeaf.java
@@ -0,0 +1,20 @@
+package easy;
+
+import easy.util.TreeNode;
+
+public class SumRootToLeaf {
+    public static boolean hasPathSum(TreeNode root, int sum) {
+        return hasPathSum(root, 0, sum);
+    }
+
+    public static boolean hasPathSum(TreeNode root, int value, int sum){
+        if(root == null){
+            return false;
+        }
+        value += root.val;
+        if(value == sum && root.left == null && root.right == null){
+            return true;
+        }
+        return (hasPathSum(root.left, value, sum) || hasPathSum(root.right, value, sum));
+    }
+}
