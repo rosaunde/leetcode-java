a915f22826a9226aa09a9be8da8c9b086cd45200
rosaunde <rosaunde@tcd.ie>
2020-08-11 20:55:44 +0100

    hello github my old friend

---
+++ src/java/easy/LongestPalindromicSubstring.java
@@ -0,0 +1,25 @@
+package easy;
+
+public class LongestPalindromicSubstring {
+    public static String longestPalindrome(String s) {
+        if(s.length() < 2){
+            return s;
+        }
+        int n = s.length();
+        String res = null;
+
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+
+                if (dp[i][j] && (res == null || j - i + 1 > res.length())) {
+                    res = s.substring(i, j + 1);
+                }
+            }
+        }
+
+        return res;
+    }
+}
