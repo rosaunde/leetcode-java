ea13f42253130acce4dab241eeab036c0bc54081
rosaunde <rosaunde@tcd.ie>
2020-08-12 12:20:15 +0100

    easiest medium ever

---
+++ src/java/easy/WaterContainer.java
@@ -0,0 +1,27 @@
+package easy;
+
+class WaterContainer {
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        while(left < right){
+            int leftHeight = height[left];
+            int rightHeight = height[right];
+            int area = (right - left) * Math.min(leftHeight, rightHeight);
+            if(area > maxArea){
+                maxArea = area;
+            }
+            if(leftHeight < rightHeight){
+                while(height[left] <= leftHeight && left < right){
+                    left++;
+                }
+            } else {
+                while(height[right] <= rightHeight && left < right){
+                    right--;
+                }
+            }
+        }
+        return maxArea;
+    }
+}
