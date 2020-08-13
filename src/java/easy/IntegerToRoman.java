d0c96560322b83994ef1ffb4ad4ffc6f48d39f61
rosaunde <rosaunde@tcd.ie>
2020-08-13 08:48:55 +0100

    kinda tricky, not really algorithmic but whatevah

---
+++ src/java/easy/IntegerToRoman.java
@@ -0,0 +1,36 @@
+package easy;
+
+import java.util.LinkedHashMap;
+
+class IntegerToRoman {
+    private LinkedHashMap<Integer, String> map = makeMap();
+    public String intToRoman(int num) {
+        String result = "";
+        for(int key : map.keySet()){
+            while(num >= key){
+                num -= key;
+                result += map.get(key);
+            }
+        }
+        return result;
+    }
+
+    private LinkedHashMap<Integer, String> makeMap(){
+        LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+
+        return map;
+    }
+}
