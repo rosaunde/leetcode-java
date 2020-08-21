30f53b3ed51f2e52ebb5dc7c38a7ac391df77825
rosaunde <rosaunde@tcd.ie>
2020-08-21 09:19:52 +0100

    cheeky easy one

---
+++ src/java/easy/RemoveLinkedListElements.java
@@ -0,0 +1,21 @@
+package easy;
+
+import easy.util.ListNode;
+
+public class RemoveLinkedListElements {
+    public static ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+        while(head != null){
+            if(head.val == val){
+                prev.next = head.next;
+                head = head.next;
+            } else{
+                prev = head;
+                head = head.next;
+            }
+        }
+        return dummy.next;
+
+    }
+}
