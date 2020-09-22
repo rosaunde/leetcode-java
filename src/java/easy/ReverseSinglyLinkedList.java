462edf6ed88378326d55a2766845251d69b7fb4b
rosaunde <rosaunde@tcd.ie>
2020-09-22 13:14:51 +0100

    back in the swing of things

---
+++ src/java/easy/ReverseSinglyLinkedList.java
@@ -0,0 +1,21 @@
+package easy;
+
+import easy.util.ListNode;
+
+public class ReverseSinglyLinkedList {
+    public static ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next = null;
+        while(current != null){
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+}
