288d2e1d805a285aadce252e4599f6c7956d70ca
rosaunde <rosaunde@tcd.ie>
2020-09-30 09:16:26 +0100

    chillin

---
+++ src/java/easy/PalindromeLinkedList.java
@@ -0,0 +1,29 @@
+package easy;
+
+import easy.util.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromeLinkedList {
+    public static boolean isPalindrome(ListNode head) {
+        List<Integer> myList = new ArrayList<>();
+        while(head != null){
+            myList.add(head.val);
+            head = head.next;
+        }
+        int left = 0;
+        int right = myList.size() - 1;
+
+        while(left<right){
+            int leftInt = myList.get(left);
+            int rightInt = myList.get(right);
+            if(leftInt != rightInt){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
