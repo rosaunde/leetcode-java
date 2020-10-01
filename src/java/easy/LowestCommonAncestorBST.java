78ec94933b127c4bec11dea494f2c5cb419be0a1
rosaunde <rosaunde@tcd.ie>
2020-10-01 09:35:12 +0100

    I like this one

---
+++ src/java/easy/LowestCommonAncestorBST.java
@@ -0,0 +1,19 @@
+package easy;
+
+import easy.util.TreeNode;
+
+public class LowestCommonAncestorBST {
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || (root.left == null && root.right == null) || root.val == p.val || root.val == q.val){
+            return root;
+        } else if((root.val > p.val) && (root.val < q.val)){
+            return root;
+        } else if((root.val < p.val) && (root.val > q.val)){
+            return root;
+        } else if(q.val > root.val && p.val > root.val){
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+    }
+}
