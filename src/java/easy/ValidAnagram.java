4b25d29f5b1186c99966c97743f0b15adaa2442a
rosaunde <rosaunde@tcd.ie>
2020-10-06 07:57:38 +0100

    easy one with a cool solution that I didn't think of

---
+++ src/java/easy/ValidAnagram.java
@@ -0,0 +1,16 @@
+package easy;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+}
