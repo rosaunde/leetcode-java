225b12a2f6372b6818b853b16b9b9bb5c7a798a0
rosaunde <rosaunde@tcd.ie>
2020-10-07 09:08:26 +0100

    bit slow this morn

---
+++ src/java/easy/BinaryTreePaths.java
@@ -0,0 +1,33 @@
+package easy;
+
+import easy.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePaths {
+    List<String> paths = new ArrayList<>();
+    private static final String SPACER = "->";
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        if(root == null){
+            return paths;
+        }
+        binaryTreePaths(root, String.valueOf(root.val));
+        return paths;
+    }
+
+    private void binaryTreePaths(TreeNode node, String path){
+        if(node.left == null && node.right == null){
+            paths.add(path);
+        } else {
+            path = path + SPACER;
+            if(node.left != null){
+                binaryTreePaths(node.left, path + node.left.val);
+            }
+            if(node.right != null){
+                binaryTreePaths(node.right, path + node.right.val);
+            }
+        }
+    }
+}
