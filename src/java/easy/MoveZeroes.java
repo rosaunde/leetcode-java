99b0fac3c2c644d6f9e4db74335ff40d9615a5a1
rosaunde <rosaunde@tcd.ie>
2020-11-11 09:05:58 +0000

    struggled big time there

---
+++ src/java/easy/MoveZeroes.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int nonZero = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                if(i != nonZero){
+                    nums[nonZero] = nums[i];
+                    nums[i] = 0;
+                }
+                nonZero++;
+            }
+        }
+    }
+}
