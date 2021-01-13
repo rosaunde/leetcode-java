5da2ad51469753386e7682fd08d5b834750ed460
rosaunde <rosaunde@tcd.ie>
2021-01-13 08:17:11 +0000

    dumb one and an easy one

---
+++ src/java/easy/ReverseCharArray.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class ReverseCharArray {
+    public void reverseString(char[] s) {
+        int leftIndex=0;
+        int rightIndex = s.length-1;
+
+        while(leftIndex < rightIndex){
+            char temp = s[leftIndex];
+            s[leftIndex] = s[rightIndex];
+            s[rightIndex] = temp;
+            leftIndex++;
+            rightIndex--;
+        }
+    }
+}
