15508d8f22e3df8a1d36a42968af62e4d5a45860
rosaunde <rosaunde@tcd.ie>
2021-02-08 10:31:39 +0000

    never used a linkedhashmap so I didn't come up with an alternative solution

---
+++ src/java/medium/LRUCache.java
@@ -0,0 +1,35 @@
+package medium;
+
+import java.util.LinkedHashMap;
+
+public class LRUCache {
+
+    public LinkedHashMap<Integer, Integer> lhm;
+    public int capacity;
+
+    public LRUCache(int capacity) {
+        this.lhm = new LinkedHashMap<Integer, Integer>(capacity);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        if(this.lhm.containsKey(key)) {
+            int retval = this.lhm.get(key);
+            this.lhm.remove(key);
+            this.put(key, retval);
+            return retval;
+        }
+        return -1;
+    }
+
+    public void put(int key, int val) {
+
+        if(this.lhm.containsKey(key)) {
+            this.lhm.remove(key);
+        } else if(this.lhm.size() == this.capacity) {
+            int firstkey = this.lhm.entrySet().iterator().next().getKey();
+            this.lhm.remove(firstkey);
+        }
+        this.lhm.put(key, val);
+    }
+}
