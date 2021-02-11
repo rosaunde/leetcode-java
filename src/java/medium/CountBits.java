51933fa97cb73c898cef6dd6c25084330dc26f62
rosaunde <rosaunde@tcd.ie>
2021-02-11 08:37:39 +0000

    amazingly beautiful solution to this one

---
+++ src/java/medium/CountBits.java
@@ -0,0 +1,10 @@
+package medium;
+
+public class CountBits {
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for (int i = 1; i <= num; ++i)
+            ans[i] = ans[i & (i - 1)] + 1;
+        return ans;
+    }
+}
