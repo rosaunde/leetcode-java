5cd751bc714eb44700fe85bd9b88233578d5008f
rosaunde <rosaunde@tcd.ie>
2021-04-15 21:51:39 +0100

    job interview stuff

--- src/java/TestMyCode.java
+++ src/java/TestMyCode.java
@@ -1,23 +1,57 @@
 import easy.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static easy.TwoSum.twoSum;
 
 
 public class TestMyCode{
     public static void main(String[] args){
-       int result = 0;
-       for(int i = 1; i < 7; i++){
-           result = result ^ i;
-       }
+      MyClass clazz = new MyClass();
+      try{
+          clazz.getValue("foo", 0);
+      } catch(Exception e){
 
-       for(int i = 2; i < 7; i++){
-           result = result ^ i;
-       }
-       System.out.println(3^3);
-       System.out.println(result);
+      }
+        try{
+            clazz.getValue("bar", 2);
+        } catch(Exception e){
+
+        }
+        try{
+            clazz.getValue("baz", 0);
+        } catch(Exception e){
+
+        }
+        try{
+            clazz.getValue("fubar", 1);
+        } catch(Exception e){
+
+        }
+
+        System.out.println(clazz.count);
+    }
+}
+
+class MyClass {
+    Map<String,Integer> map;
+    public int count = 0;
+
+    public MyClass(){
+        this.map = new HashMap<>();
+        map.put("foo", 1);
+        map.put("bar", 3);
+    }
+
+    public int getValue(String input, int retries) {
+        count = count + 1;
+        try{
+            return map.get(input);
+        } catch(Exception e){
+            if(retries > 3){
+                throw e;
+            }
+        }
+        return getValue(input, retries + 1);
     }
 }
